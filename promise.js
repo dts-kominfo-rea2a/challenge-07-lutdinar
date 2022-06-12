94779c0db0c6617d6746bdb7328f7c530f46b25f
Lut Dinar Fadila <lutdinarfadila10@gmail.com>
2022-06-12 18:18:26 +0700

    add function promiseOutput get result total emotions

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let ixx = await promiseTheaterIXX();
+    let vgc = await promiseTheaterVGC();
+
+    let dataArr = ixx.concat(vgc);
+    return dataArr.filter((obj) => obj.hasil === emosi).length;
+
+    // return emotionArr.filter((data) => data.hasil === emosi).lenght;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
